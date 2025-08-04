6e855f8b9cea85b5746144c892d41026d7af059c
imran Niaz <iamimranniaz@outlook.com>
2025-08-04 09:45:24 -0700

    remiove frmo License check initiated

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\TradeDataBinder::class,
-            \App\Http\Middleware\LicenseMiddleware::class,
+            // \App\Http\Middleware\LicenseMiddleware::class,
             
         ],
 
